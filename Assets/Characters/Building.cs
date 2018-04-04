463109684f493c559f8fd09f4db727c300b82eab
Simon Broggi <simon.broggi@insert-coin.ch>
2018-04-04 08:13:53 +0200

    buildings respawn when walking out of world

---
+++ Assets/Characters/Building.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.z <= -14f){
+			Vector3 odlPos = transform.position;
+			transform.position = new Vector3(odlPos.x, odlPos.y, 14f);
+		}
+	}
+}
