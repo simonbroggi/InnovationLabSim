9fa8bee40280278e37ff5a98b91a6d7d745e6d0b
Simon Broggi <simon.broggi@insert-coin.ch>
2018-04-05 09:23:48 +0200

    restart scene

---
+++ Assets/Restart.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Restart : MonoBehaviour, IPointerClickHandler {
+	public void OnPointerClick(PointerEventData eventData) {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+}
