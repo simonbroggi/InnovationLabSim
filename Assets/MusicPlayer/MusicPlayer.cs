10f67f77625c0b22a12eeca1fefe748a3f97113b
Simon Broggi <simon.broggi@insert-coin.ch>
2018-04-06 10:18:43 +0200

    music player

---
+++ Assets/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioSource[] audioSources;
+	
+	public int audioPlayingIndex = -1;
+	private int lastAudioIndex = -1;
+
+	void Update () {
+		if(audioPlayingIndex >= 0 && audioPlayingIndex < audioSources.Length) {
+			if(audioPlayingIndex != lastAudioIndex) {
+				foreach(AudioSource audioSource in audioSources) {
+					audioSource.volume = 0f;
+				}
+				audioSources[audioPlayingIndex].volume = 1f;
+			}
+		}
+	}
+}
